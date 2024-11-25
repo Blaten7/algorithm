91959078997f35073f46f28861a65d873c92071d
LHG <162647372+Blaten7@users.noreply.github.com>
2024-11-26 07:50:09 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,15 @@
+interface Main {
+    static void main(String[] x) {
+        var s = new java.util.Scanner(System.in);
+        int a = s.nextInt(), b = s.nextInt(), c = s.nextInt();
+        System.out.println(a + b + c != 180 ? "Error" :
+                a == 60 && b == 60 && c == 60 ? "Equilateral" :
+                        a == b || b == c || a == c ? "Isosceles" : "Scalene");
+
+    }
+}
+
+
+
+
+

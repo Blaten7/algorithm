3b6d8410c0c4f3eb04cd1d8818346afdea3678dc
LHG <162647372+Blaten7@users.noreply.github.com>
2024-11-27 06:12:47 +0900

    [Bronze II] Title: 모스 부호, Time: 112 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/29701. 모스 부호/모스 부호.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        /*
+            문제
+            모스부호를 받아서 변환 전 문자열을 만들기.
+
+            모스부호와 관련된 표준 라이브러리가 존재하지 않음
+            바로 생각나는건
+            하나하나 값 추가해놓고 불러와서 합치는것 뿐
+         */
+        int testCases = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap<String, String> map = new HashMap<>();
+        map.put(".-", "A");
+        map.put("-...", "B");
+        map.put("-.-.", "C");
+        map.put("-..", "D");
+        map.put(".", "E");
+        map.put("..-.", "F");
+        map.put("--.", "G");
+        map.put("....", "H");
+        map.put("..", "I");
+        map.put(".---", "J");
+        map.put("-.-", "K");
+        map.put(".-..", "L");
+        map.put("--", "M");
+        map.put("-.", "N");
+        map.put("---", "O");
+        map.put(".--.", "P");
+        map.put("--.-", "Q");
+        map.put(".-.", "R");
+        map.put("...", "S");
+        map.put("-", "T");
+        map.put("..-", "U");
+        map.put("...-", "V");
+        map.put(".--", "W");
+        map.put("-..-", "X");
+        map.put("-.--", "Y");
+        map.put("--..", "Z");
+        map.put(".----", "1");
+        map.put("..---", "2");
+        map.put("...--", "3");
+        map.put("....-", "4");
+        map.put(".....", "5");
+        map.put("-....", "6");
+        map.put("--...", "7");
+        map.put("---..", "8");
+        map.put("----.", "9");
+        map.put("-----", "0");
+        map.put("--..--", ",");
+        map.put(".-.-.-", ".");
+        map.put("..--..", "?");
+        map.put("---...", ":");
+        map.put("-....-", "-");
+        map.put(".--.-.", "@");
+        while (testCases-- > 0) {
+            String input = st.nextToken();
+            bw.write(map.get(input));
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+}

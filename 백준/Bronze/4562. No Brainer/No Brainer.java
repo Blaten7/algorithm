c3a6f0583c7f9d0c4b19cf1c775d7944d7f95144
LHG <162647372+Blaten7@users.noreply.github.com>
2024-11-27 05:43:18 +0900

    [Bronze IV] Title: No Brainer, Time: 168 ms, Memory: 17188 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,5 @@
+interface Main{
+    static void main(String[]x){
+        var s=new java.util.Scanner(System.in);
+        int t=s.nextInt();
+        while(t-->0)System.out.println(s.nextInt()>=s.nextInt()?"MMM BRAINS":"NO BRAINS");}}

a88d228f425f469d47604e0a25ac6857d01a9a9e
LHG <162647372+Blaten7@users.noreply.github.com>
2024-11-27 06:07:57 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 88 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,17 @@
+interface Main{
+    static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int testCase = sc.nextInt()*2;
+//        int current = 0;
+//        int k = sc.nextInt();
+//        current += k;
+//        while (testCase-- > 0) {
+//            int i = sc.nextInt();
+//            int j = sc.nextInt();
+//            current += i;
+//            current -=j;
+//        }
+        System.out.println("비와이");
+
+    }
+}

a237725595c213a2ebd594b9392c1e11789482bb
LHG <162647372+Blaten7@users.noreply.github.com>
2024-11-26 10:02:15 +0900

    [Bronze IV] Title: 과자, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/10156. 과자/과자.java
+++ 백준/Bronze/10156. 과자/과자.java
@@ -1,7 +1,1 @@
-interface Main {
-    static void main(String[] x) {
-        var s = new java.util.Scanner(System.in);
-        int a=s.nextInt()*s.nextInt()-s.nextInt();
-        System.out.println(a>0?a:0);
-    }
-}
+interface Main{static void main(String[]x){var s=new java.util.Scanner(System.in);int a=s.nextInt()*s.nextInt()-s.nextInt();System.out.println(a>0?a:0);}}

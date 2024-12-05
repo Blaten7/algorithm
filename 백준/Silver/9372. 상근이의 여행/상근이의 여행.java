85f8f6cc20f021b2b2b3c12f0db95f03936a2014
LHG <162647372+Blaten7@users.noreply.github.com>
2024-12-05 21:07:30 +0900

    [Silver IV] Title: 상근이의 여행, Time: 784 ms, Memory: 239304 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,1 @@
+interface Main{static void main(String[]x){var s=new java.util.Scanner(System.in);int a=s.nextInt();while(a-->0){int b=s.nextInt(),c=s.nextInt();s.nextLine();while(c-->0)s.nextLine();System.out.println(b-1);}}}
